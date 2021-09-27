c0bbc8571594ad995418b9a0871fc8c1bd23bfeb
Amreshdev <84233605+Amreshdev@users.noreply.github.com>
2021-09-27 12:24:54 +0530

    Delete login.js

--- js/login.js
+++
@@ -1,8 +0,0 @@
-function addUser(){
-          var user = document.getElementById("user_name").value;
-          console.log(user);
-
-          localStorage.setItem("User name", user);
-          window.location.href = "lets_chat_room.html" ;
-
-}
